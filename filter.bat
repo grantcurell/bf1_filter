dec41ea030750273df581f578ae49e7bb342ca33
Grant Curell <35169230+grantcurell@users.noreply.github.com>
2018-03-31 10:27:47 -0500

    Update filter.bat

--- filter.bat
+++ filter.bat
@@ -61,3 +61,5 @@ netsh advfirewall firewall add rule name="BF1 Block List" dir=in interface=any a
 netsh advfirewall firewall add rule name="BF1 Block List" dir=out interface=any action=block remoteip=54.93.68.173/32
 netsh advfirewall firewall add rule name="BF1 Block List" dir=in interface=any action=block remoteip=52.59.238.145/32
 netsh advfirewall firewall add rule name="BF1 Block List" dir=out interface=any action=block remoteip=52.59.238.145/32
+netsh advfirewall firewall add rule name="BF1 Block List" dir=in interface=any action=block remoteip=31.204.143.144/32
+netsh advfirewall firewall add rule name="BF1 Block List" dir=out interface=any action=block remoteip=31.204.143.144/32
